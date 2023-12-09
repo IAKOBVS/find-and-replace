458b4c3383e608e69d97411ed9f666523a785798
James Tirta Halim <tirtajames45@gmail.com>
2023-12-10 02:36:47 +0700

    new

--- find-and-replace.c
+++ find-and-replace.c
@@ -51,7 +51,9 @@ typedef enum {
 } print_mode_ty;
 
 typedef struct global_ty {
+	const char *file_pattern;
 	int print_mode;
+	int recursive;
 } global_ty;
 global_ty G = { 0 };
 
@@ -93,8 +95,14 @@ process_file(jstr_ty *R buf,
              const char *R rplc,
              const size_t rplc_len)
 {
+	jstrio_ft_ty ft = jstrio_exttype(buf->data, buf->size);
+	if (ft == JSTRIO_FT_BINARY)
+		return JSTR_RET_SUCC;
 	if (jstr_chk(jstrio_freadfile_len_j(buf, fname, "r", file_size)))
 		goto err;
+	if (ft == JSTRIO_FT_UNKNOWN)
+		if (jstr_isbinary(buf->data, 64, buf->size))
+			return JSTR_RET_SUCC;
 	const size_t changed = jstr_rplcall_len_j(buf, find, find_len, rplc, rplc_len);
 	if (changed == (size_t)-1)
 		goto err;
@@ -121,14 +129,12 @@ err:
 	JSTR_RETURN_ERR(JSTR_RET_ERR);
 }
 
-#if 0
-
 typedef struct args_ty {
 	jstr_ty *buf;
 	const char *find;
+	size_t find_len;
 	const char *rplc;
 	size_t rplc_len;
-	size_t find_len;
 } args_ty;
 
 static JSTRIO_FTW_FUNC(callback_file, ftw, args)
@@ -141,29 +147,6 @@ err:
 	JSTR_RETURN_ERR(JSTR_RET_ERR);
 }
 
-static jstr_ret_ty
-process_dir(jstr_ty *R buf,
-            const char *R dir,
-            const char *R find,
-            const size_t find_len,
-            const char *R rplc,
-            const size_t rplc_len)
-{
-	args_ty args;
-	args.buf = buf;
-	args.find = find;
-	args.find_len = find_len;
-	args.rplc = rplc;
-	args.rplc_len = rplc_len;
-	if (jstr_chk(jstrio_ftw(dir, callback_file, &args, JSTRIO_FTW_STATREG | JSTRIO_FTW_REG | JSTRIO_FTW_DIR, NULL, 0)))
-		goto err;
-	return JSTR_RET_SUCC;
-err:
-	JSTR_RETURN_ERR(JSTR_RET_ERR);
-}
-
-#endif
-
 int
 main(int argc, char **argv)
 {
@@ -174,7 +157,9 @@ main(int argc, char **argv)
 		         "Options:\n"
 		         "-i, -i.bak\n"
 		         "Instead of printing to stdout, replace the files in-place.\n"
-		         "If .bak is provided, backup the original file prefixed with .bak.\n",
+		         "If .bak is provided, backup the original file prefixed with .bak.\n"
+		         "-r\n"
+		         "Recurse on the arguments given if they are directories.\n",
 		         argv[0]);
 		return EXIT_FAILURE;
 	}
@@ -183,13 +168,28 @@ main(int argc, char **argv)
 	struct stat st;
 	const size_t find_len = strlen(FIND);
 	const size_t rplc_len = strlen(RPLC);
-	for (unsigned int i = 3; argv[i]; ++i) {
+	for (unsigned int i = 3; ARG; ++i) {
 		if (G.print_mode == 0) {
-			if (!strcmp(argv[i], "-i.bak")) {
+			if (!jstr_strcmpeq_loop(ARG, "-i.bak")) {
 				G.print_mode = PRINT_FILE_BACKUP;
 				continue;
-			} else if (!strcmp(argv[i], "-i")) {
+			} else if (!jstr_strcmpeq_loop(ARG, "-i")) {
 				G.print_mode = PRINT_FILE;
+				continue;
+			}
+		}
+		if (G.recursive == 0) {
+			if (!jstr_strcmpeq_loop(ARG, "-r")) {
+				G.recursive = 1;
+				continue;
+			}
+		}
+		if (G.file_pattern == NULL) {
+			if (!jstr_strcmpeq_loop(ARG, "-name")) {
+				++i;
+				if (jstr_nullchk(ARG))
+					jstr_errdie("No argument after -name flag.");
+				G.file_pattern = ARG;
 				continue;
 			}
 		}
@@ -197,10 +197,17 @@ main(int argc, char **argv)
 		DIE_IF(ret == JSTR_RET_ERR);
 		if (ret != JSTR_RET_SUCC)
 			continue;
-		if (IS_REG(st.st_mode))
+		if (IS_REG(st.st_mode)) {
 			DIE_IF(jstr_chk(process_file(&buf, ARG, (size_t)st.st_size, FIND, find_len, RPLC, rplc_len)));
-		else
+		} else if (IS_DIR(st.st_mode)) {
+			if (G.recursive) {
+				args_ty a = { &buf, FIND, find_len, RPLC, rplc_len };
+				DIE_IF(!jstr_chk(jstrio_ftw(ARG, callback_file, &a, JSTRIO_FTW_REG | JSTRIO_FTW_STATREG, G.file_pattern, 0)));
+			}
+		} else {
 			PRINTERR("stat() failed on %s.\n", ARG);
+			exit(EXIT_FAILURE);
+		}
 	}
 	jstr_free_j(&buf);
 	return EXIT_SUCCESS;
